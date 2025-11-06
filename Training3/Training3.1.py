7e20bc3626e72a90938937435bb8204d5624146f
danielribeiro <danielribeiro@iesmontsia.org>
2025-11-06 16:52:42 +0100

    Commit3

---
+++ Training3/Training3.1.py
@@ -0,0 +1,6 @@
+# Programa que demana l'edat de l'usuari i indica si és menor o adult
+edat = int(input("Introdueix la teva edat: "))
+if edat < 18:
+    print("Ets menor d'edat.")
+else:
+    print("Ets adult.")
---
+++ Training3/Training3.3.py
@@ -0,0 +1,6 @@
+num1 = int(input("Introdueix un nombre: "))
+if num1 >= 0:
+    print("El nombre és positiu.")
+else:
+    print("El nombre és negatiu.")
+# Programa que demana un nombre i indica si és positiu, negatiu o zero
